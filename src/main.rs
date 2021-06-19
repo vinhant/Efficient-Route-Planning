fa48be8e2b30114336c65e5144e759a61dc4abd5
Vinh-An Trinh <vinhant@gmail.com>
2021-06-18 23:49:54 -0700

    Add ASCII version of lecture graph

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@
 #[derive(Debug)]
 struct Node {
   // The OSM id of the node.
-  osmId: i32,
+  osm_id: i32,
 
   // The latitude and longitude.
   latitude: f64,
@@ -29,15 +29,15 @@ struct Node {
 #[derive(Debug)]
 struct Arc {
   // The id of the head node.
-  headNodeId: i32,
+  head_node_id: i32,
 
   // The cost of the arc = travel time in seconds (see class comment above).
   cost: i32,
 }
 
 impl Arc {
-    pub fn new(headNodeId: i32, cost: i32) -> Arc {
-        Arc { headNodeId, cost }
+    pub fn new(head_node_id: i32, cost: i32) -> Arc {
+        Arc { head_node_id, cost }
     }
 }
 
@@ -50,15 +50,15 @@ struct RoadNetwork {
   // PRIVATE members.
   
   // The number of nodes in the graph.
-  numNodes: i32,
+  num_nodes: i32,
 
   // The number of (undirected) edges in the graph.
-  numEdges: i32,
+  num_edges: i32,
 
   // The adjacency lists. Note that each edge {u,v} is stored as two arcs: (u,v)
   // and (v,u). The total number of entries in these arrays is therefore exactly twice
   // the number of edges in the graph.
-  adjacentArcs: Vec<Vec<Arc>>,
+  adjacent_arcs: Vec<Vec<Arc>>,
 
   // The nodes of the graph.
   nodes: Vec<Node>,
@@ -69,30 +69,62 @@ impl RoadNetwork {
  
   // Create an empty network (with zero nodes and zero arcs).
   pub fn new() -> RoadNetwork {
-      RoadNetwork { numNodes: 0, numEdges: 0, adjacentArcs: vec!(vec!()), nodes: vec!()}
+      RoadNetwork { num_nodes: 0, num_edges: 0, adjacent_arcs: vec!(vec!()), nodes: vec!()}
   }
 
   // Add a node with the given OSM id and lat/lng coordinates.
-  pub fn addNode(osmId: i32, latitude: f32, longitude: f32) {
+  pub fn add_node(&mut self, osm_id: i32, latitude: f64, longitude: f64) {
+      self.nodes.push( Node { osm_id, latitude, longitude });
   }
 
   // Add an (undirected) edge between the given nodes with the given cost.
-  pub fn addEdge(u: i32, v: i32, cost: i32) {
+  pub fn add_edge(&mut self, u: i32, v: i32, cost: i32) {
   }
 
   // Read graph from given OSM file.
-  pub fn readFromOsmFile(fileName: &str) {
+  pub fn read_from_osm_file(&mut self, filename: &str) {
   }
 
 }
 
 fn main() {
-    let n = Node {   osmId: 0, latitude: 0.0, longitude: 0.0, };
+    let n = Node {   osm_id: 0, latitude: 0.0, longitude: 0.0, };
     println!("Node: {:?}", n);
 
     let a = Arc::new(0, 0);
     println!("Arc: {:?}", a);
 
-    let rn = RoadNetwork::new();
+    let mut rn = RoadNetwork::new();
+    rn.add_node(1, 2.2, 3.3);
     println!("RoadNetwork: {:?}", rn);
 }
+
+//
+// ASCII graph of the slide
+// (1) ----3---- (2)
+// \            / |
+//  \          /  |
+//  1         1   |
+//   \       /    3
+//    \     /     |
+//      (3)       |
+//                |
+// (4)----5------(5)
+//
+// Array<Array<Arc>>
+// 1 -> [ Arc{nodeId: 2, cost: 3}, Arc{nodeId: 3, cost: 1}]
+// 2 -> [ Arc{nodeId: 1, cost: 3}, Arc{nodeId: 3, cost: 1}, Arc{nodeId: 5, cost: 3}]
+// 3 -> [ Arc{nodeId: 1, cost: 1}, Arc{nodeId: 2, cost: 1}]
+// 4 -> [ Arc{nodeId: 5, cost: 5}]
+// 5 -> [ Arc{nodeId: 2, cost: 3}, Arc{nodeId: 4, cost: 5}]
+
+// add_node(100, 0.0, 0.0); add_node(200, 0.0, 0.0); ... add_node(500, 0.0, 0.0);
+//   nodes: [ (100, 0.0, 0.0), (200, 0.0, 0.0), (300, 0.0, 0.0), (400, 0.0, 0.0), (500, 0.0, 0.0)]
+//   nodes[0] is node 1, nodes[1] is node 2, etc in ASCII graph above
+//
+// add_edge(u, v, cost)
+//   adjacentArcs[u].push(Arc{nodeId: v, cost})
+//   adjacentArcs[v].push(Arc{nodeId: u, cost})
+//   To add edge from node 1 to 2 with cost 3 in ASCII graph above:
+//   add_edge(0, 1, 3);
+//   // 
